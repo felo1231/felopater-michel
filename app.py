38ccf6f08766961f10097c6067e05bfb378e77fb
felo1231 <fero1231fero@gmail.com>
2026-06-10 18:57:42 +0000

    4542542

--- app.py
+++ app.py
@@ -335,10 +335,10 @@ st.markdown("""
     }
 
     /* 2. فرض اللون الأبيض على كافة النصوص والعناوين */
-     p{
+    div{
         color: #000000 !important;
     }
-    h1, h2, h3, h4, label, div, span, l1 {
+    h1, h2, h3, h4, label, p, span, l1 {
         color: #ffffff !important;
     }
 
