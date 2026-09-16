6bd7a54f3d3d52b56bb8e8d43db41b006122601a
felo1231 <fero1231fero@gmail.com>
2026-09-16 12:56:58 +0000

    ghkjljkhj

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ except ImportError:
 st.markdown("""
     <style>
     .stApp {
-        background: linear-gradient(-45deg, #22abbd, #2293bd, #227cbd, #2265bd, #2243bd, #2231bd);
+        background: linear-gradient(-45deg, #2265bd, ##224ebd, #2241bd, #2232bd, #2235bd, #222abd, #2722bd, #3922bd);
         background-size: 400% 400%;
         animation: gradient 15s ease infinite;
     }
